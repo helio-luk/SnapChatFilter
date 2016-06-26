8e1734e0505b3c21c702869629fa5e54f8fd313c
Emily Souza <emily.souza.rs@gmail.com>
2016-06-26 17:48:34 -0300

    Landmakrs e alguns resultados

--- coletaDePontos.m
+++ coletaDePontos.m
@@ -1,8 +1,8 @@
 clear all
 close all
 
-% a = imread('imgFiltro/coelho.pnag');
-a = imread('imgFace/avril2.jpg');
+% a = imread('imgFiltro/gato3.jpg');
+a = imread('imgFace/grupo2.jpg');
 
 imshow(a);
 [x, y] = getpts;
@@ -10,44 +10,58 @@ imshow(a);
 fix([x y]);
 [l,~] = size(x);
 
-arq = fopen('Landmarks-face/landmark-face-avril2.txt','w');
+% %faces
+% arq = fopen('Landmarks-face/landmark-face-zac.txt','w');
+% 
+% %oculos
+% fprintf(arq,'fixedPoints = [');
+% for i = 1:(l-1)
+%     fprintf(arq,'%d ', uint16(x(i)));
+%     fprintf(arq,'%d; ', uint16(y(i)));
+% end
+% fprintf(arq,'%d ', uint16(x(l)));
+% fprintf(arq,'%d]; ', uint16(y(l)));
+% 
+% [x, y] = getpts;
+% 
+% fix([x y]);
+% [l,~] = size(x);
+% 
+% %fucinho
+% fprintf(arq,'fixedPoints = [');
+% for i = 1:(l-1)
+%     fprintf(arq,'%d ', uint16(x(i)));
+%     fprintf(arq,'%d; ', uint16(y(i)));
+% end
+% fprintf(arq,'%d ', uint16(x(l)));
+% fprintf(arq,'%d]; ', uint16(y(l)));
+% 
+% [x, y] = getpts;
+% 
+% fix([x y]);
+% [l,~] = size(x);
+% 
+% %orelhas
+% fprintf(arq,'fixedPoints = [');
+% for i = 1:(l-1)
+%     fprintf(arq,'%d ', uint16(x(i)));
+%     fprintf(arq,'%d; ', uint16(y(i)));
+% end
+% fprintf(arq,'%d ', uint16(x(l)));
+% fprintf(arq,'%d]; ', uint16(y(l)));
+% [x, y] = getpts;
+% 
+% fix([x y]);
+% [l,~] = size(x);
+% 
+% %fucinho menor
+% fprintf(arq,'fixedPoints = [');
+% for i = 1:(l-1)
+%     fprintf(arq,'%d ', uint16(x(i)));
+%     fprintf(arq,'%d; ', uint16(y(i)));
+% end
+% fprintf(arq,'%d ', uint16(x(l)));
+% fprintf(arq,'%d]; ', uint16(y(l)));
 
-%oculos
-fprintf(arq,'fixedPoints = [');
-for i = 1:(l-1)
-    fprintf(arq,'%d ', uint16(x(i)));
-    fprintf(arq,'%d; ', uint16(y(i)));
-end
-fprintf(arq,'%d ', uint16(x(l)));
-fprintf(arq,'%d]; ', uint16(y(l)));
-
-[x, y] = getpts;
-
-fix([x y]);
-[l,~] = size(x);
-
-%fucinho
-fprintf(arq,'fixedPoints = [');
-for i = 1:(l-1)
-    fprintf(arq,'%d ', uint16(x(i)));
-    fprintf(arq,'%d; ', uint16(y(i)));
-end
-fprintf(arq,'%d ', uint16(x(l)));
-fprintf(arq,'%d]; ', uint16(y(l)));
-
-[x, y] = getpts;
-
-fix([x y]);
-[l,~] = size(x);
-
-%orelhas
-fprintf(arq,'fixedPoints = [');
-for i = 1:(l-1)
-    fprintf(arq,'%d ', uint16(x(i)));
-    fprintf(arq,'%d; ', uint16(y(i)));
-end
-fprintf(arq,'%d ', uint16(x(l)));
-fprintf(arq,'%d]; ', uint16(y(l)));
-
-fclose(arq);
+% fclose(arq);
 close all
--- main.m
+++ main.m
@@ -2,13 +2,25 @@ clear all
 close all
 
 
-imagemRosto = imread('imgFace/helio.png');
-mascara     = imread('imgMascara/cachorro1Focinho.png');
-imagemFiltro= imread('imgFiltro/cachorro1.png');
+imagemRosto = imread('imgFace/grupo2.jpg');
+mascara     = imread('imgMascara/oculos1m.png');
+imagemFiltro= imread('imgFiltro/oculos1.png');
 
-fixedPoints=[251   344; 342   340;  293   307;  293   367];
-movingPoints = [144   255; 243   251;  190   216;  194   268];
+fixedPoints = [831 263; 819 254; 795 304; 780 296];
+movingPoints = [660 411; 660 539; 1468 419; 1464 549];
 
 x = transformacao(imagemRosto, imagemFiltro, mascara, fixedPoints, movingPoints);
+
+fixedPoints = [840 552; 816 548; 828 597; 807 597];
+
+x = transformacao(x, imagemFiltro, mascara, fixedPoints, movingPoints);
+
+fixedPoints = [951 690; 969 704; 993 650; 1008 662];
+
+x = transformacao(x, imagemFiltro, mascara, fixedPoints, movingPoints);
+
+fixedPoints = [1089 371; 1107 380; 1113 329; 1128 329];
+
+x = transformacao(x, imagemFiltro, mascara, fixedPoints, movingPoints);
 
 imshow(x);
